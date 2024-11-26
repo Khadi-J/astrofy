4bff2fc23c1c764253bd14ff441a23749f186ac8
Khadi-J <khadijahakinz1@gmail.com>
2024-11-26 13:52:15 +0100

    Update config.ts
    description limit

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Khadijah Akintoye| A Digital Marketing Expert';
-export const SITE_DESCRIPTION = 'Khadijah is a professional digital marketer with over 4years experience. Specializing in SEO, Social media marketing, content creating, image and video editing.';
+export const SITE_DESCRIPTION = 'Khadijah is a professional digital marketer with over 4years experience. Specializing in SEO, Social media marketing and content creating.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
